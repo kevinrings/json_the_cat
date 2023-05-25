c128ca069d7a25cd2134d0ab24de532295fcac9e
kevinrings <wnkevin@gmail.com>
2023-05-25 04:18:55 -0700

    export fetchBreedDescription function and implement test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,25 @@
 const request = require('request');
-const breedName = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
-    } else {
-      console.log(data[0].description);
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
     }
-  }
-});
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback('Breed not found', null);
+      return;
+    }
+
+    const description = data[0].description;
+
+    callback(null, description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
